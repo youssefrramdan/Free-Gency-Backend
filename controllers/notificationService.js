e277d697ecb9766e05e2dd5c84279386e995cd83
youssefrramdan <youfahmed353@gmail.com>
2025-04-30 03:11:52 +0300

    chore: update .gitignore to include Firebase service account JSON file

--- controllers/notificationService.js
+++ controllers/notificationService.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import admin from '../firebaseInit.js';
+import admin from '../firebase/firebase.js';
 import ApiError from '../utils/apiError.js';
 
 /**
